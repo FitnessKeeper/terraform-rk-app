b7503cdbeca1c6da1627fa413d8615b8f501a84a
Steve Huff <steve.huff@runkeeper.com>
2017-08-01 17:58:15 -0400

    done tweaking

--- main.tf
+++ main.tf
@@ -16,48 +16,42 @@ data "aws_vpc" "vpc" {
   id = "${var.vpc_id}"
 }
 
-data "aws_alb" "app" {
-  arn = "${var.alb_arn}"
-  name = "${var.alb_name}"
-}
-
 data "aws_ami" "app" {
-  most_recent = true
-  executable_users = ["self"]
-  owners = ["${data.aws_caller_identity.current.account_id}"]
+  most_recent      = true
+  owners           = ["${data.aws_caller_identity.current.account_id}"]
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["${var.ami_name}"]
   }
 }
 
 resource "aws_autoscaling_group" "app" {
-  name-prefix = "tf-asg-${data.aws_vpc.vpc.tags["Name"]}-${var.app}-"
-  max_size = "${var.max_size}"
-  min_size = "${var.min_size}"
-  desired_capacity = "${var.desired_capacity}"
+  name_prefix               = "tf-asg-${data.aws_vpc.vpc.tags["Name"]}-${var.app}-"
+  max_size                  = "${var.max_size}"
+  min_size                  = "${var.min_size}"
+  desired_capacity          = "${var.desired_capacity}"
   health_check_grace_period = "${var.health_check_grace_period}"
-  health_check_type = "${var.health_check_type}"
-  termination_policies = ["OldestLaunchConfiguration", "ClosestToNextInstanceHour", "Default"]
+  health_check_type         = "${var.health_check_type}"
+  termination_policies      = ["OldestLaunchConfiguration", "ClosestToNextInstanceHour", "Default"]
   wait_for_capacity_timeout = "${var.wait_for_capacity_timeout}"
-  metrics_granularity = "1Minute"
-  enabled_metrics = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
-  target_group_arns = ["${aws_alb_target_group.app.id}"]
-  launch_configuration = "${aws_launch_configuration.app.name}"
-  vpc_zone_identifier = ["${var.asg_subnets}"]
+  metrics_granularity       = "1Minute"
+  enabled_metrics           = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
+  target_group_arns         = ["${aws_alb_target_group.app.id}"]
+  launch_configuration      = "${aws_launch_configuration.app.name}"
+  vpc_zone_identifier       = ["${var.asg_subnets}"]
 
-  tags [
+  tags = [
     {
-      key = "Name"
-      value = "tf-asg-${data.aws_vpc.vpc.tags["Name"]}-${var.app}-"
+      key                 = "Name"
+      value               = "tf-asg-${data.aws_vpc.vpc.tags["Name"]}-${var.app}-"
       propagate_at_launch = true
     },
     {
-      key = "App"
-      value = "${var.app}"
+      key                 = "App"
+      value               = "${var.app}"
       propagate_at_launch = true
-    }
+    },
   ]
 
   lifecycle {
@@ -66,12 +60,12 @@ resource "aws_autoscaling_group" "app" {
 }
 
 resource "aws_launch_configuration" "app" {
-  name-prefix = "tf-lc-${data.aws_vpc.vpc.tags["Name"]}-${var.app}-"
-  image_id = "${data.aws_ami.app.id}"
-  instance_type = "${var.instance_type}"
-  iam_instance_profile = "${var.iam_instance_profile}"
-  key_name = "${var.key_name}"
-  security_groups = ["${var.security_groups}"]
+  name_prefix                 = "tf-lc-${data.aws_vpc.vpc.tags["Name"]}-${var.app}-"
+  image_id                    = "${data.aws_ami.app.id}"
+  instance_type               = "${var.instance_type}"
+  iam_instance_profile        = "${var.iam_instance_profile}"
+  key_name                    = "${var.key_name}"
+  security_groups             = ["${aws_security_group.app.id}"]
   associate_public_ip_address = false
 
   lifecycle {
@@ -80,17 +74,18 @@ resource "aws_launch_configuration" "app" {
 }
 
 resource "aws_alb_target_group" "app" {
-  name-prefix = "tf-tg-${data.aws_vpc.vpc.tags["Name"]}-${var.app}-"
-  port = "${var.port}"
-  protocol = "${var.app_protocol}"
-  vpc_id = "${data.aws_vpc.vpc.id}"
+  name        = "tf-tg-${data.aws_vpc.vpc.tags["Name"]}-${var.app}"
+  port        = "${var.port}"
+  protocol    = "${var.protocol}"
+  vpc_id      = "${data.aws_vpc.vpc.id}"
 
   health_check {
-    path = "${health_check_path}"
-    interval = "${var.health_check_interval}"
-    timeout = "${var.health_check_timeout}"
-    healthy_threshold = "${var.health_check_healthy_threshold}"
+    path                = "${var.health_check_path}"
+    interval            = "${var.health_check_interval}"
+    timeout             = "${var.health_check_timeout}"
+    healthy_threshold   = "${var.health_check_healthy_threshold}"
     unhealthy_threshold = "${var.health_check_unhealthy_threshold}"
+    matcher             = "${var.health_check_healthy_codes}"
   }
 
   lifecycle {
@@ -99,40 +94,43 @@ resource "aws_alb_target_group" "app" {
 }
 
 resource "aws_security_group" "app" {
-  name-prefix = "tf-sg-${data.aws_vpc.vpc.tags["Name"]}-${var.app}-"
+  name_prefix = "tf-sg-${data.aws_vpc.vpc.tags["Name"]}-${var.app}-"
   description = "${var.app} application server security group"
   vpc_id      = "${data.aws_vpc.vpc.id}"
 }
 
 resource "aws_security_group_rule" "app_ingress_http" {
-  type = "ingress"
-  from_port = "80"
-  to_port = "80"
-  source_security_group_id = "${var.alb_security_group}"
-  security_group_id = "${aws_security_group.app.id}"
+  type                     = "ingress"
+  from_port                = "80"
+  to_port                  = "80"
+  protocol                 = "tcp"
+  source_security_group_id = "${var.alb_security_group_id}"
+  security_group_id        = "${aws_security_group.app.id}"
 }
 
 resource "aws_security_group_rule" "app_ingress_https" {
-  type = "ingress"
-  from_port = "443"
-  to_port = "443"
-  source_security_group_id = "${var.alb_security_group}"
-  security_group_id = "${aws_security_group.app.id}"
+  type                     = "ingress"
+  from_port                = "443"
+  to_port                  = "443"
+  protocol                 = "tcp"
+  source_security_group_id = "${var.alb_security_group_id}"
+  security_group_id        = "${aws_security_group.app.id}"
 }
 
 resource "aws_security_group_rule" "app_ingress_ssh" {
-  type = "ingress"
-  from_port = "22"
-  to_port = "22"
-  source_security_group_id = "${var.bastion_security_group}"
-  security_group_id = "${aws_security_group.app.id}"
+  type                     = "ingress"
+  from_port                = "22"
+  to_port                  = "22"
+  protocol                 = "tcp"
+  source_security_group_id = "${var.bastion_security_group_id}"
+  security_group_id        = "${aws_security_group.app.id}"
 }
 
 resource "aws_security_group_rule" "app_egress" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 65535
-  protocol    = "all"
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "all"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.app.id}"
 }
--- variables.tf
+++ variables.tf
@@ -1,140 +1,136 @@
 variable "app" {
-  type = "string"
+  type        = "string"
   description = "Name of application"
 }
 
 variable "ami_name" {
-  type = "string"
+  type        = "string"
   description = "Name of AMI to deploy via launch configuration"
 }
 
 variable "vpc_id" {
-  type = "string"
+  type        = "string"
   description = "VPC ID in which to provision resources"
 }
 
-variable "alb_arn" {
-  type = "string"
-  description = "ARN of ALB to which target group should be registered (must provide this or alb_name)"
-  default = ""
-}
-
-variable "alb_name" {
-  type = "string"
-  description = "Name of ALB to which target group should be registered (must provide this or alb_arn)"
-  default = ""
-}
-
-variable "role_name" {
-  type = "string"
-  description = "Name of IAM role under which app server instances should run"
+variable "alb_listener_arn" {
+  type        = "string"
+  description = "ARN of ALB listener to which target group should be registered"
+  default     = ""
 }
 
 variable "max_size" {
-  type = "string"
+  type        = "string"
   description = "Maximum size of auto scaling group"
-  default = "4"
+  default     = "4"
 }
 
 variable "min_size" {
-  type = "string"
+  type        = "string"
   description = "Minimum size of auto scaling group"
-  default = "2"
+  default     = "2"
 }
 
 variable "desired_capacity" {
-  type = "string"
+  type        = "string"
   description = "Desired capacity at creation of auto scaling group"
-  default = "2"
+  default     = "2"
 }
 
 variable "health_check_grace_period" {
-  type = "string"
+  type        = "string"
   description = "Time in seconds to wait before beginning health checks"
-  default = 120
+  default     = 120
 }
 
 variable "health_check_type" {
-  type = "string"
+  type        = "string"
   description = "Type of health check to apply"
-  default = "ELB"
+  default     = "ELB"
 }
 
 variable "wait_for_capacity_timeout" {
-  type = "string"
+  type        = "string"
   description = "Time in seconds to wait for capacity changes to complete"
-  default = "600"
+  default     = "10m"
 }
 
 variable "asg_subnets" {
-  type = "list"
+  type        = "list"
   description = "List of subnet IDs in which to create app server instances"
 }
 
 variable "instance_type" {
-  type = "string"
+  type        = "string"
   description = "Type of EC2 instance to use for app servers"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "iam_instance_profile" {
-  type = "string"
+  type        = "string"
   description = "ARN of IAM profile to associate with app server instances"
 }
 
 variable "key_name" {
-  type = "string"
+  type        = "string"
   description = "EC2 key name to be used for access to app server instances"
 }
 
 variable "alb_security_group_id" {
-  type = "list"
+  type        = "string"
   description = "Security group ID associated with load balancer"
 }
 
 variable "bastion_security_group_id" {
-  type = "list"
+  type        = "string"
   description = "Security group ID associated with bastion host"
 }
 
 variable "port" {
-  type = "string"
+  type        = "string"
   description = "Numeric port on which the application listens"
-  default = "8080"
+  default     = "8080"
 }
 
 variable "protocol" {
-  type = "string"
+  type        = "string"
   description = "Protocol on which the application listens"
-  default = "HTTP"
+  default     = "HTTP"
 }
 
 variable "health_check_path" {
-  type = "string"
+  type        = "string"
   description = "URL path for HTTP health check"
-  default = "/healthcheck"
+  default     = "/healthcheck"
 }
 
 variable "health_check_interval" {
-  type = "string"
+  type        = "string"
   description = "Interval in seconds for load balancer health checks"
-  default = "15"
+  default     = "15"
 }
 
 variable "health_check_timeout" {
-  type = "string"
+  type        = "string"
   description = "Time in seconds before load balancer health check times out"
-  default = "5"
+  default     = "5"
 }
 
 variable "health_check_healthy_threshold" {
-  type = "string"
+  type        = "string"
   description = "Number of consecutive successful health checks before service is healthy"
-  default = "2"
+  default     = "2"
 }
 
 variable "health_check_unhealthy_threshold" {
-  type = "string"
+  type        = "string"
   description = "Number of consecutive failed health checks before service is unhealthy"
-  default = "4"
+  default     = "4"
 }
+
+variable "health_check_healthy_codes" {
+  type        = "string"
+  description = "HTTP response codes to be considered healthy"
+  default     = "200,302"
+}
+
