ffb29392d056ec2e0cd8e9f23e4314af95d60c4a
Tim Hartmann <tfhartmann@gmail.com>
2018-04-12 15:36:02 -0400

    Adding tg name to outputs

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "target_group_arn_suffix" {
   value = "${aws_alb_target_group.app.arn_suffix}"
 }
 
+output "target_group_name" {
+  value = "${aws_alb_target_group.app.name}"
+}
+
 output "ami_name" {
   value = "${data.aws_ami.app.name}"
 }
