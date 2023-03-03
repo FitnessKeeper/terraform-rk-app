1cfa81aba730b2255cca7539c9ff63e69c497e56
Sean Haggerty <sean.haggerty@raceroster.com>
2023-03-03 13:06:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_autoscaling_group" "app" {
 
 resource "aws_launch_configuration" "app" {
   name_prefix                 = "tf-lc-${data.aws_vpc.vpc.tags["Name"]}-${var.app}-"
-  image_id                    = var.ami_id ? null : data.aws_ami.app.id
+  image_id                    = var.ami_id ? null : data.aws_ami.app.id[0]
   instance_type               = var.instance_type
   iam_instance_profile        = var.iam_instance_profile
   key_name                    = var.key_name
