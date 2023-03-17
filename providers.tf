b6729dd1434384a4656c8dc9820f8a50541ca4c0
Sai_prasanna <saiprasanna.gopularam@raceroster.com>
2023-03-17 16:47:18 -0400

    Update terraform and aws versions

--- providers.tf
+++ providers.tf
@@ -1,10 +1,18 @@
+terraform {
+  required_version = "~> 1.0.11"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.46.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 2.46.0"
   alias   = "us-west-1"
 }
 
 provider "aws" {
-  version = "~> 2.46.0"
   alias   = "us-east-1"
 }
-
