a7c0bc2d44a0cc9475fdd24c5dad019954f4a4ed
SaiPrasannaGopularam <110479454+SaiPrasannaGopularam@users.noreply.github.com>
2023-03-20 14:10:55 -0400

    Update aws to 3.75.2 and terraform to 1.4.2 (#14)
    * Update to 3.75.2
    
    * Update terraform version constraint

--- providers.tf
+++ providers.tf
@@ -1,18 +1,10 @@
 terraform {
-  required_version = "~> 1.0.11"
+  required_version = "~> 1.4.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.46.0"
+      version = "~> 3.75.2"
     }
   }
-}
-
-provider "aws" {
-  alias   = "us-west-1"
-}
-
-provider "aws" {
-  alias   = "us-east-1"
 }
