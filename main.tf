48117e94f6f7ce3b154e9d56b1300d166c1b0c17
SaiPrasannaGopularam <110479454+SaiPrasannaGopularam@users.noreply.github.com>
2023-03-21 13:16:47 -0400

    Update insecure_value to be var.ami_id (#17)

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_ssm_parameter" "ami_id_param" {
   name           = "/${var.env}/${var.app}/webapi_ami_id"
   description    = "AMI ID to be used for webapi_secondary/tertiary instances"
   type           = "String"
-  insecure_value = var.ami_id == "" ? var.ami_id : null
+  insecure_value = var.ami_id
 }
 
 data "aws_ami" "app" {
